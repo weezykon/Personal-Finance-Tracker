1fa192bb440e71473b653a95529acca89dad855e
Akinbode Akinlolu <weezykon@gmail.com>
2025-11-11 15:30:03 +0100

    chore: setup tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import postcss from './postcss.config.js'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  css: {
+    postcss,
+  },
 })
 
